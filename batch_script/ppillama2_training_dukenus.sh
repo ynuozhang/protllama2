5000a7b52a1d153029836b76386de1aee8f79b5d
ynuozhang <yzhang@u.duke.nus.edu>
2023-10-24 18:16:02 +0800

    Revert "standalone training code minor fix"
    This reverts commit eb114f876f7af2d4820db3ba4b2c8e00c2df7bb0.

--- batch_script/ppillama2_training_dukenus.sh
+++ batch_script/ppillama2_training_dukenus.sh
@@ -13,7 +13,7 @@ DATA_LOC=$HOME_LOC/data/ppi_8000
 OUTPUT_LOC=$HOME_LOC/data/ppi_8000
 mkdir $OUTPUT_LOC
 SCRIPT_LOC=$HOME_LOC/protllama
-TOKENIZER_LOC=$HOME_LOC/protllama/batch_script/
+TOKENIZER_LOC=$HOME_LOC/data/tokenizers/
 LOG_LOC=$HOME_LOC/protllama/batch_script/logs
 mkdir $LOG_LOC
 image=/data/rozen/home/e0833634/distributed_containers/update_centos7_old_container_v2.sif
@@ -31,7 +31,7 @@ printf pwd
 python3.8 $SCRIPT_LOC/bin/main.py \
   ${DATE}_dukenus_hpc \
   ppi \
-  4 \
+  3 \
   $DATA_LOC \
   $OUTPUT_LOC \
   $TOKENIZER_LOC \
@@ -41,13 +41,13 @@ python3.8 $SCRIPT_LOC/bin/main.py \
   --num_key_value_heads 20 \
   --batch_size 1 \
   --max_position_embeddings 1024 \
-  --vocab_size 10k \
+  --vocab_size 32k \
   --hidden_size 1280 \
   --intermediate_size 3440 \
-  --devices 2 \
+  --devices 1 \
   --accumulate_grad_batches 10 \
   --strategy ddp \
-  --flash_attention True > ${DATE}_dukenus_hpc_ppi_pretrain_set7_10percent_singlegpu.log 2>&1
+  --flash_attention True > ${DATE}_dukenus_hpc_ppi_pretrain_set6_full_ddp.log 2>&1
 
 exit
 
